4e51953a51a151721cdafd6c8becc94645dc8ecb
alexismaskio <alexismaskio@gmail.com>
2023-10-04 13:26:14 +0200

    test

---
+++ ApiDotNet/Extensions/LoggingConfigurationExtension.cs
@@ -0,0 +1,16 @@
+namespace ApiDotNet.Extensions;
+
+/// <summary>
+/// Extension permettant de configurer les logs de l'application
+/// </summary>
+public static class LoggingConfigurationExtension
+{
+    /// <summary>
+    /// Configure les logs de l'application
+    /// </summary>
+    /// <param name="builder"></param>
+    public static void ConfigureLogging(this WebApplicationBuilder builder)
+    {
+        
+    }
+}
